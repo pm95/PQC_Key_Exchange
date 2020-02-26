ce6ff4a70fb28007a84654fc0aa78f90ca61b63a
Pietro Malky <pietromalky@gmail.com>
2020-02-26 12:41:39 -0600

    structs work fine. renamed some variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,22 +4,20 @@
 struct Person
 {
     //private variable, do not peak
-    int p;
+    int private_key;
 
     // public key
-    int P;
+    int public_key;
 
     // shared secret with another person
-    int p_ss;
+    int shared_secret;
 };
 
 int mod_pow(int base, int exp, int n)
 {
     int result = 1;
-
     for (int i = 1; i <= exp; i++)
         result = fmod(result * fmod(base, n), n);
-
     return result;
 }
 
@@ -36,21 +34,21 @@ int main()
     struct Person Bob;
 
     // define private keys for Alice and Bob
-    Alice.p = 1523;
-    Bob.p = 1597;
+    Alice.private_key = 1523;
+    Bob.private_key = 1597;
 
     // calculate public keys for Alice and Bob
-    Alice.P = mod_pow(g, Alice.p, n);
-    Bob.P = mod_pow(g, Bob.p, n);
+    Alice.public_key = mod_pow(g, Alice.private_key, n);
+    Bob.public_key = mod_pow(g, Bob.private_key, n);
 
     // calculate shared secret for Alice and Bob
-    Alice.p_ss = mod_pow(Bob.P, Alice.p, n);
-    Bob.p_ss = mod_pow(Alice.P, Bob.p, n);
+    Alice.shared_secret = mod_pow(Bob.public_key, Alice.private_key, n);
+    Bob.shared_secret = mod_pow(Alice.public_key, Bob.private_key, n);
 
     printf("\n\n");
-    printf("n: %d\ng: %d\n\nAlice private key, a: %d\nBob private key, b: %d\n\n", n, g, Alice.p, Bob.p);
-    printf("Alice public key: %d\nBob public key: %d\n\n", Alice.P, Bob.P);
-    printf("Alice SS: %d\nBob SS: %d\n\n", Alice.p_ss, Bob.p_ss);
+    printf("n: %d\ng: %d\n\nAlice private key, a: %d\nBob private key, b: %d\n\n", n, g, Alice.private_key, Bob.private_key);
+    printf("Alice public key: %d\nBob public key: %d\n\n", Alice.public_key, Bob.public_key);
+    printf("Alice SS: %d\nBob SS: %d\n\n", Alice.shared_secret, Bob.shared_secret);
     printf("\n\n");
     return 0;
 }
