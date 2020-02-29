0b93924987276cbb85ebc17a8e0aba83dc6f7a35
Pietro Malky <pietromalky@gmail.com>
2020-02-29 13:35:55 -0600

    got client server TCP code from geeks for geeks now need to send public key information

--- Alice.c
+++ Alice.c
@@ -1,8 +1,75 @@
 #include "Public.h"
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+// additional imports
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+void func(int sockfd)
+{
+    char buff[MAX];
+    int N;
+    for (;;)
+    {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        N = 0;
+        while ((buff[N++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        printf("From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0)
+        {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
 
 int main()
 {
     printf("Hello from Alice");
 
-    return 0;
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+    {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
 }
