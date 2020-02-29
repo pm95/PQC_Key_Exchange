910aab849613b85139b81c3ffdc4211f14bcab37
Pietro Malky <pietromalky@gmail.com>
2020-02-29 13:37:17 -0600

    changed somthing related to unnamed prototype parameters

--- Public.h
+++ Public.h
@@ -18,10 +18,10 @@ struct Person
     int shared_secret;
 };
 
-int mod_pow(int base, int exp, int n)
+int mod_pow(int base, int exp, int N)
 {
     int result = 1;
     for (int i = 1; i <= exp; i++)
-        result = fmod(result * fmod(base, n), n);
+        result = fmod(result * fmod(base, N), N);
     return result;
 }
